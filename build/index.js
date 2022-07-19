5fec92c0752273658cf756743911eae83d437e62
Elybourland <JEBourland@yahoo.com>
2022-07-19 10:48:41 -0700

    configuring the ts compiler

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('Hi there!');
+// tsc -w in cmd terminal to watch/compile code into index.js
+const logSomething = () => {
+    console.log('something');
+};
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log('Hi there!');
+// tsc -w in cmd terminal to watch/compile code into index.js
+
+const logSomething = () => {
+  console.log('something');
+};
