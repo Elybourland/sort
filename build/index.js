bbaf4cda9194565f38ae9a023a641459e44d6e88
Elybourland <JEBourland@yahoo.com>
2022-07-20 14:05:28 -0700

    concurrently compilation and execution

--- build/index.js
+++ build/index.js
@@ -1,6 +1,7 @@
 "use strict";
-console.log('Hi there!');
+console.log(123);
 // tsc -w in cmd terminal to watch/compile code into index.js
 const logSomething = () => {
     console.log('something');
 };
+logSomething();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-console.log('Hi there!');
+console.log('123');
 // tsc -w in cmd terminal to watch/compile code into index.js
 
 const logSomething = () => {
   console.log('something');
 };
+
+logSomething();
