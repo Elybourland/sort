32ae01c8baaf0afd4d89024ea7adf5c9727e200e
Elybourland <JEBourland@yahoo.com>
2022-07-21 15:01:23 -0700

    separating swapping and comparison

--- build/index.js
+++ build/index.js
@@ -1,4 +1,8 @@
 "use strict";
-const sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const sorter_1 = require("./sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+const sorter = new sorter_1.Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
